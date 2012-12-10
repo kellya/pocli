a7bcae96a13bfeb00346b0a62a8a1a929bc505cd
Alex Kelly <alex.kelly@franklin.edu>
2012-12-10 16:53:03 -0500

    Base release with everything functional

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 pocli
 =====
 
-A generic command line interface for PushOver.net
+A generic command line interface for PushOver.net.  This should fully support
+all the options that pushover.net api allows, and I will try to keep it
+updated should the API change.
+
+Requirements
+============
+
+You need to have
+  * An existing account w/ pushover.net
+  * An Application on pushover.net created
+  * Python (2.7 or better.  2.6 might work, don't really know)
+
+Running
+=======
+
+After you have the requirements, and have added your specific data to the conf
+file, you just need to run *pocli.py --message*.  Additinally, you may run
+*pocli.py --help* to see the list of options available.
+
+Warnings
+========
+
+This is the first time I have tried to write a python app.  I am doing this
+mostly to learn Python.  As such, I am trying to do things correctly, but may
+not always do it in the best pyton way, so if you want to modify the code the
+way I have done something might be aggravating.  Sorry, I'm learning :)
+
+Contact
+=======
+If you find any problems, want to contribute code, whatever:  Please channel 
+all communications through github (https://github.com/kellya/pocli)
+
+License
+=======
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- pocli.py
+++ pocli.py
@@ -7,16 +7,21 @@ import sys
 import urllib
 import urllib2
 
+# Set the defaults, these shouldn't really change, but could be moved to
+# the config file if that makes sense
+# msgUrl = the URL used to post messages to pushover API
+# devUrl = the URL for device validation to pushover API
+# sndUrl = the URL to get the list of sounds supportted by the pushover API
+# cfFile = the default location for the config file
+# FIXME: The cfFile could be a dictionary of various likely places instead of
+#        one, hard-coded value
 defaults = {
     ('msgUrl'):'https://api.pushover.net/1/messages.json',
     ('devUrl'):'https://api.pushover.net/1/users/validate.json',
     ('sndUrl'):'https://api.pushover.net/1/sounds.json',
-    ('cfFile'):'./pocli.cfg'
+    ('cfFile'):'./pocli.cfg',
 }
 
-#These two variables are oddly out of place since this script grew.  FIXME
-#url = "https://api.pushover.net/1/messages.json"
-http = httplib2.Http()
 
 def read_config(conf):
   """
@@ -24,19 +29,23 @@ def read_config(conf):
   global variables for them.  This may be better to return a dict
   I'll look into that later FIXME
   """
-  global user
-  global token
+  # Create an empty dictionary that will be appended w/ the values
+  values={}
   try:
     config = ConfigParser.RawConfigParser()
     config.read(conf)
-    token = config.get('Main', 'token')
+    values['token'] = config.get('Main', 'token')
     user = config.get('Main', 'user')
+    values['user'] = config.get('Main', 'user')
+    if config.has_option('Main', 'device'):
+      values['device'] = config.get('Main', 'device')
   except ConfigParser.NoSectionError, err:
     print 'Error:  Couldn\'t read config'
     sys.exit(2)
   except ConfigParser.ParsingError, err:
     print 'Couldn\'t parse config file: ', err
     sys.exit(99)
+  return values
 
 def get_sounds():
   "This gets a list of sounds from a JSON call, and outputs it as a string"
@@ -45,23 +54,24 @@ def get_sounds():
   sounds_url.close()
   all_sounds =""
   if sound_list['status'] != 1:
-    all_sounds = "Error: Could not retreive list of sounds"
+    all_sounds = "Error: Could not retrieve list of sounds"
   else:
     for sounds, value in sound_list['sounds'].iteritems():
       all_sounds=all_sounds + ' ' + sounds
   return all_sounds
 
-def dev_is_valid(checkdev):
+def dev_is_valid(checkdev, checkuser, checktoken):
   """
+  FIXME!!!
+  This section isn't currently called, and is broken
   Return True or False based on validation of the device passed
   through checkdev.
   """
   # This is the URL defined in the API docs at 
   # https://pushover.net/api#identifiers
-#  url = 'https://api.pushover.net/1/users/validate.json'
   params = urllib.urlencode({
-    'user': user,
-    'token': token,
+    'user': checkuser,
+    'token': checktoken,
     'device': checkdev
   })
 
@@ -79,55 +89,95 @@ def build_params ():
   """Builds parameters and returns a dictionary
   well ok, fine.  It only throws them in a dictionary w/o logic at the moment
   """
-  return {
-      'user': user,
-      'token': token,
-      'title': args.title,
-      'message': args.message,
-      'sound': args.sound,
-      'priority': args.priority,
-      'url': args.url
-  }
+  #start with an empty dictionary, and build what we will eventually return
+  #FIXME returnval is a bad name since I use it in non-returny ways w/ device
+  returnval={}
+  parser = argparse.ArgumentParser(description='Process command line options')
+  parser.add_argument('-f', '--config', dest='config',
+                      help='Config file for persistent options (defaults to '
+                      + defaults['cfFile'])
+  parser.add_argument('-m', '--message', dest='message', required=True,
+                      help='''Specify the message contents.  Needs to be in
+                      quotes if you have spaces''')
+  parser.add_argument('-s', '--sound', dest='sound',
+                      help='''Name of the sound, which can be any of the 
+                      following: '''+ get_sounds())
+  parser.add_argument('-k', '--token', dest='token', 
+                      help='''Application API token (Required, but may be 
+                      specified in config)''')
+  parser.add_argument('-t', '--title', dest='title',
+                      help='The title of the message')
+  parser.add_argument('-u', '--url', dest='url',
+                      help="Additional URL to append to the message")
+  parser.add_argument('-p', '--priority', dest='priority',
+                      help='''Priority of the message.  Can be 1 for high, or -1
+                      for silent''')
+  parser.add_argument('-d', '--device', dest='devid',
+                      help="The specific device to send the message")
+  parser.add_argument('-n', '--user', dest='user',
+                      help="User information")
+  parser.add_argument('--timestamp', dest='epochval',
+                      help="""Timestamp to set timestamp to a value other than
+                      now.  This is passed in epoch format""")
+  args = parser.parse_args()
+  
+  
+  if args.token is not None and args.user is not None:
+    #if we were given a token and a user information on the commandline, lets
+    #use those
+    returnval['token']=args.token
+    returnval['user']=args.user
+  elif args.config is not None:
+    #otherwise, we'll try to get them from the config
+    returnval=read_config(args.config)
+  else:
+    #Well they've got to specified somewhere, so die if we get here
+    print """
+    Error:
+    user and token are required.  They either BOTH need to be specified on the
+    command line, or BOTH in the config file.  For more information run:
+    """
+    print sys.argv[0] + " --help\n"
+    sys.exit(1)
+    
+  # Message is required, so argparse is already going to validate it exists
+  returnval['message'] = args.message
+  
+  #I am doing essentially the same thing twice.  This needs cleaned up FIXME
+  if args.devid is not None:
+    if dev_is_valid(args.devid,returnval['user'],returnval['token']):
+      returnval['device'] = args.devid
+    else:
+      print "Error: Device " + str(args.devid) + " is invalid"
+      sys.exit(1)
+  if 'device' in returnval:
+    #We got a device from config, let's validate it
+    if not dev_is_valid(returnval['device'],returnval['user'],returnval['token']):
+      print "Error: Device " + str(returnval['device']) + " is invalid"
+      sys.exit(1)
+      
+  # It feels like there may be a better way to loop through this next stuff
+  if args.sound is not None:
+    returnval['sound'] = args.sound
+    
+  if args.priority is not None:
+    returnval['priority'] = args.priority
+    
+  if args.title is not None:
+    returnval['title'] = args.title
+  
+  if args.url is not None:
+    returnval['url'] = args.url
+  
+  if args.epochval is not None:
+    returnval['timestamp'] = args.epochval
+  
+  return returnval
 
-
-parser = argparse.ArgumentParser(description='Process command line options')
-parser.add_argument('-f', '--config', dest='config',
-    help='Config file for persistent options (defaults to '+ defaults['cfFile'])
-parser.add_argument('-m', '--message', dest='message', required=True,
-    help='Specify the message contents.  Needs to be in quotes if you have spaces')
-parser.add_argument('-s', '--sound', dest='sound',
-    help='Name of the sound, which can be any of the following: '+ get_sounds())
-parser.add_argument('-k', '--token', dest='token', 
-    help='Application API token (Required, but may be specified in config)')
-parser.add_argument('-t', '--title', dest='title',
-    help='The title of the message')
-parser.add_argument('-u', '--url', dest='url',
-    help="Additional URL to append to the message")
-parser.add_argument('-p', '--priority', dest='priority',
-    help="Priority of the message.  Can be 1 for high, or -1 for silent")
-parser.add_argument('-d', '--device', dest='devid',
-    help="The specific device to send the message")
-args = parser.parse_args()
-
-if args.config is None:
-  read_config('./pocli.cfg')
-else:
-  read_config(args.config)
-
-if args.devid is not None:
-  dev_is_valid(args.devid)
-
-params = urllib.urlencode(build_params())
-#params = urllib.urlencode({
-#  'user': user,
-#  'token': token,
-#  'title': args.title,
-#  'message': args.message,
-#  'sound': args.sound,
-#  'priority': args.priority,
-#  'url': args.url
-#})
-
-response, content = http.request(defaults['msgUrl'], 'POST', params,
-    headers={'Content-Type': 'application/x-www-form-urlencoded'}
+http = httplib2.Http()
+# This is where we actually make the call to send the message
+response, content = http.request(defaults['msgUrl'], 'POST', 
+                                 urllib.urlencode(build_params()),
+                                 headers={'Content-Type': 
+                                          'application/x-www-form-urlencoded'}
 )
